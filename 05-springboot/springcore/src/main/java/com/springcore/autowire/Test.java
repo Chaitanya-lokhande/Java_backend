9fb4a83b12ddd777cb074d1dc31d8564784e01dc
Chaitanya-lokhande <lokhandechaitanya44@gmail.com>
2026-08-13 00:54:38 +0530

    Autowiring in spring

---
+++ 05-springboot/springcore/src/main/java/com/springcore/autowire/Test.java
@@ -0,0 +1,14 @@
+package com.springcore.autowire;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ApplicationContext context =  new ClassPathXmlApplicationContext("com/springcore/autowire/config.xml");
+		Employee emp1 = (Employee) context.getBean("emp1");
+		System.out.println(emp1);
+	}
+
+}
